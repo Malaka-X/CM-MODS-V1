54bcf13cdd047184f69cd9f3b30861cfc7e26aa2
Malaka-X <malakamdbot@gmail.com>
2025-01-28 12:38:05 +0530

    Update malaka-md.js

--- malaka-md.js
+++ malaka-md.js
@@ -1,3 +1,3 @@
 {
-"megafile" : "https://mega.nz/file/jR8HwJ7B#kM3zUu9dNnUjDzIqkIqjUXt1UdZx3SX-qGHIc-eomIA"
+"zipmegalink" : "https://mega.nz/file/jR8HwJ7B#kM3zUu9dNnUjDzIqkIqjUXt1UdZx3SX-qGHIc-eomIA"
 }
